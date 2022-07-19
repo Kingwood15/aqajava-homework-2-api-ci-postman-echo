0a49d5b6b8b33fc440958f817eb9e36f185bb3d0
Азат Ильдарханов <defcon797@gmail.com>
2022-07-19 22:14:48 +0300

    Initial commit

---
+++ src/test/java/ru/netology/postmanEchoTest.java
@@ -0,0 +1,47 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class postmanEchoTest {
+
+    @Test
+    void shouldPostmanEchoApiFirstTest() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                //.body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("ru.netology.api.postman")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                //.body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("ru.netology.api.postman"))
+        ;
+    }
+
+    @Test
+    void shouldPostmanEchoApiSecondTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Мой дядя самых честных правил,\n" +
+                        "Когда не в шутку занемог,\n" +
+                        "Он уважать себя заставил\n" +
+                        "И лучше выдумать не мог.")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Мой дядя самых честных правил,\n" +
+                        "Когда не в шутку занемог,\n" +
+                        "Он уважать себя заставил\n" +
+                        "И лучше выдумать не мог."))
+        ;
+    }
+}
